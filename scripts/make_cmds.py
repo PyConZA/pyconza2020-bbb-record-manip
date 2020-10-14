7ac8ca13e43de50cce2c69edd4d7a1c3900bddd9
Neil Muller <drnlmuller@gmail.com>
2020-10-14 12:16:45 +0200

    update cmds script

--- scripts/make_cmds.py
+++ scripts/make_cmds.py
@@ -3,6 +3,7 @@
 # Turn a list of 'name' - 'url' lines into a set of pastable commands
 
 with open('list') as f:
-   for line in f.readlines():
-      name, url = line.strip().split(' - ')
-      print(f'docker run -ti -v /tmp/output:/data pyconza-bbb-export "{url}" "{name}" 0 true')
+    print('#!/bin/bash')
+    for line in f.readlines():
+        name, url = line.strip().split(' - ')
+        print(f'docker run -ti -v /tmp/output:/data pyconza-bbb-export "{url}" "{name}_fixed" 0 true')
